9b850efad63fee4d1ca02ff1f8353dcb22980057
Darrius Serrant <darrius@ironsoftware.com>
2024-12-26 10:16:22 +0700

    Update Program.java

--- how-to/java-merge-pdf-tutorial/src/main/java/com/ironsoftware/ironpdfjava/examples/howTo/javaMergePdfTutorial/Program.java
+++ how-to/java-merge-pdf-tutorial/src/main/java/com/ironsoftware/ironpdfjava/examples/howTo/javaMergePdfTutorial/Program.java
@@ -6,12 +6,10 @@ import com.ironsoftware.ironpdf.*;
 public class Program {
 
 	public static void main(String[] args) {
-
 		Section1.run();
 		// Section2.run();
 		// Section3.run();
 		// Section4.run();
-
 	}
 
 }
